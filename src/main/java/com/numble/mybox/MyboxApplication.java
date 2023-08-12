d09b6fc9ecca58d7cbf75b5c9b43722c48046f85
tajon1030 <da33202@gmail.com>
2023-08-12 18:29:10 +0900

    init

---
+++ src/main/java/com/numble/mybox/MyboxApplication.java
@@ -0,0 +1,13 @@
+package com.numble.mybox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/numble/mybox/MyboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numble.mybox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
